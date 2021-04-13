d9c171b8ffef2870608900ca93754ddbff7fccf9
huangiyongqi <860029955@qq.com>
2021-04-13 20:47:34 +0800

    update

--- packages/sd-ui/packages/sd-transfer/transfer-sptions.vue
+++ packages/sd-ui/packages/sd-transfer/transfer-sptions.vue
@@ -114,14 +114,8 @@ export default {
     
     // 定位
     setPosition () {
-      console.log(this.parentDom, 'parentDom')
-      
-      // let _top = this.getOffset(this.parentDom, 'top') + 50
       let _top = this.parentDom.getBoundingClientRect().y + 50
-      
       let _left = this.parentDom.getBoundingClientRect().x
-
-      console.log(this.parentDom.getBoundingClientRect().y, _top, _left, '----')
 
       this.transferStyle = {
         top: _top + 'px',
